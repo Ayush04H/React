06bb1c3bef03798e2bb83fca2355133882e68444
Ayush04H <ayush050419@gmail.com>
2026-07-06 23:54:24 +0530

    The Delete Functionality basis Id

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,27 +1,27 @@
-import { use, useState } from "react";
+import { useState } from "react";
 import "./App.css";
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "T-Shirts", quantity: 8, packed: true },
-  { id: 4, description: "Jeans", quantity: 3, packed: false },
-  { id: 5, description: "Jackets", quantity: 2, packed: true },
-  { id: 6, description: "Shoes", quantity: 2, packed: false },
-  { id: 7, description: "Toothbrush", quantity: 2, packed: true },
-  { id: 8, description: "Toothpaste", quantity: 1, packed: false },
-  { id: 9, description: "Phone Charger", quantity: 2, packed: true },
-  { id: 10, description: "Laptop", quantity: 1, packed: false },
-  { id: 11, description: "Laptop Charger", quantity: 1, packed: true },
-  { id: 12, description: "Headphones", quantity: 1, packed: false },
-  { id: 13, description: "Power Bank", quantity: 1, packed: true },
-  { id: 14, description: "Water Bottle", quantity: 2, packed: false },
-  { id: 15, description: "Sunglasses", quantity: 2, packed: true },
-  { id: 16, description: "Umbrella", quantity: 1, packed: false },
-  { id: 17, description: "Medicines", quantity: 5, packed: true },
-  { id: 18, description: "Travel Pillow", quantity: 2, packed: false },
-  { id: 19, description: "Camera", quantity: 1, packed: true },
-  { id: 20, description: "Snacks", quantity: 10, packed: false },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "T-Shirts", quantity: 8, packed: true },
+//   { id: 4, description: "Jeans", quantity: 3, packed: false },
+//   { id: 5, description: "Jackets", quantity: 2, packed: true },
+//   { id: 6, description: "Shoes", quantity: 2, packed: false },
+//   { id: 7, description: "Toothbrush", quantity: 2, packed: true },
+//   { id: 8, description: "Toothpaste", quantity: 1, packed: false },
+//   { id: 9, description: "Phone Charger", quantity: 2, packed: true },
+//   { id: 10, description: "Laptop", quantity: 1, packed: false },
+//   { id: 11, description: "Laptop Charger", quantity: 1, packed: true },
+//   { id: 12, description: "Headphones", quantity: 1, packed: false },
+//   { id: 13, description: "Power Bank", quantity: 1, packed: true },
+//   { id: 14, description: "Water Bottle", quantity: 2, packed: false },
+//   { id: 15, description: "Sunglasses", quantity: 2, packed: true },
+//   { id: 16, description: "Umbrella", quantity: 1, packed: false },
+//   { id: 17, description: "Medicines", quantity: 5, packed: true },
+//   { id: 18, description: "Travel Pillow", quantity: 2, packed: false },
+//   { id: 19, description: "Camera", quantity: 1, packed: true },
+//   { id: 20, description: "Snacks", quantity: 10, packed: false },
+// ];
 function App() {
   return (
     <>
@@ -35,11 +35,15 @@ function Main() {
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
   }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
   return (
     <div>
       <Logo />
       <Form handleAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} handleDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
@@ -88,25 +92,25 @@ function Form({ handleAddItems }) {
     </form>
   );
 }
-function PackingList({ items }) {
+function PackingList({ items, handleDeleteItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} key={item.id} handleDeleteItem={handleDeleteItem} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, handleDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => handleDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
