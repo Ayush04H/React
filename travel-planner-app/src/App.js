a84f41bcdc4264739d92d686e957dc4b093f268c
Ayush04H <ayush050419@gmail.com>
2026-07-10 01:40:36 +0530

    Main File for Travel Planner

---
+++ travel-planner-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TravelDashboard from "./TravelDashboard";
+
+function App() {
+  return (
+    <div>
+      <TravelDashboard />
+    </div>
+  );
+}
+
+export default App;
