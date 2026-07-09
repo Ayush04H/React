84d8eeba5115a266c1c1121bce4aa83294536225
Ayush04H <ayush050419@gmail.com>
2026-07-10 01:41:50 +0530

    MTravel Dashboard File for Travel Planner

---
+++ travel-planner-app/src/TravelDashboard.js
@@ -0,0 +1,16 @@
+function TravelDashboard() {
+  return (
+    <div>
+      <TripViewer />
+    </div>
+  );
+}
+function TripViewer() {
+  return (
+    <div>
+      <TripCard />
+    </div>
+  );
+}
+function TripCard() {}
+export default TravelDashboard;
