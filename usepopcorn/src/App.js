beb3b9890d2004b4038bfb87234ecc28f551974e
Ayush04H <ayush050419@gmail.com>
2026-07-16 11:52:11 +0530

    Splitted MovieList adn WatchedMovieList

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./index.css";
 
+//Temp Movie Data
 const tempMovieData = [
   {
     imdbID: "tt1375666",
@@ -24,7 +25,7 @@ const tempMovieData = [
       "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
   },
 ];
-
+//Temp Watched Data
 const tempWatchedData = [
   {
     imdbID: "tt1375666",
