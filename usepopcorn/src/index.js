ee64ed7010cbc0c6b987c3ef475d677cde62e75d
Ayush04H <ayush050419@gmail.com>
2026-07-17 13:29:29 +0530

    Feat:Made the Star Dynamic

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App";
 
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setmovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="green" maxRating={10} onSetRating={setmovieRating} />
+      <p>This movies has a Rating of {movieRating}</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
     <StarRating maxRating={5} />
+    <Test />
   </React.StrictMode>,
 );
