abdb0b8eaee2d785b6de6b7486abc251a5b0ce9c
Ayush04H <ayush050419@gmail.com>
2026-08-18 11:17:15 +0530

    Resfactoring

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,29 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App";
+import App from "./App";
 
-import StarRating from "./StarRating";
+// import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setmovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="green" maxRating={10} onSetRating={setmovieRating} />
-      <p>This movies has a Rating of {movieRating}</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRating, setmovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="green" maxRating={10} onSetRating={setmovieRating} />
+//       <p>This movies has a Rating of {movieRating}</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
       defaultRating={3}
     />
     <StarRating maxRating={5} />
-    <Test />
+    <Test /> */}
   </React.StrictMode>,
 );
